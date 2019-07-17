d72f90548f2b97f8b2f5d87d9dd4779b619c66f7
Razvan Mihaescu <razvan.mihaescu@student.upt.ro>
2019-07-17 15:04:45 +0300

    Created two simple HTML pages.

---
+++ myScript.js
@@ -0,0 +1,6 @@
+document.getElementById("btnSubmit").addEventListener('click', function (){
+    alert('Comanda a fost trimisa!')
+})
+document.getElementById("btnAbout").addEventListener('click', function (){
+    window.location.href = "/about.html";
+})
